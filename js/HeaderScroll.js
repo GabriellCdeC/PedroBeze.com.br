3c40ed1678be3640133db657796f38ff5c6568a3
Gabriell Castro de Carvalho <49693579+GabriellCdeC@users.noreply.github.com>
2020-09-01 14:34:07 -0300

    Header and slider update

--- js/HeaderScroll.js
+++ js/HeaderScroll.js
@@ -1,11 +1,23 @@
 const headerElement = document.querySelector('#conteudo .header')
 const headerImg = document.querySelector('#conteudo .header_img img')
 
+
 function modifySizeOnScroll(){
-    window.addEventListener('scroll' , function(event){
-     
-        headerImg.style.height= '12vh'
+
+
+    window.addEventListener('scroll' , function(event){      
+
+        var scrollPos = window.scrollY || window.scrollTop || document.getElementsByTagName("html")[0].scrollTop
+        
+
+        if(scrollPos > 0){
+            headerImg.style.height= '12vh'
+        } else if (scrollPos == 0){
+            headerImg.style.height= '15vh'
+        }      
     })
+
+    
 }
 
 modifySizeOnScroll()

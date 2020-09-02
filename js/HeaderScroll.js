049a8a1d896114b984a0311ff440d7e8d1ca7c6f
Gabriell Castro de Carvalho <49693579+GabriellCdeC@users.noreply.github.com>
2020-09-02 17:47:46 -0300

    Update HeaderScroll.js

--- js/HeaderScroll.js
+++ js/HeaderScroll.js
@@ -1,23 +1,18 @@
-const headerElement = document.querySelector('#conteudo .header')
-const headerImg = document.querySelector('#conteudo .header_img img')
+"use strict";
 
+var headerElement = document.querySelector('#conteudo .header');
+var headerImg = document.querySelector('#conteudo .header_img img');
 
-function modifySizeOnScroll(){
+function modifySizeOnScroll() {
+  window.addEventListener('scroll', function (event) {
+    var scrollPos = window.scrollY || window.scrollTop || document.getElementsByTagName("html")[0].scrollTop;
 
-
-    window.addEventListener('scroll' , function(event){      
-
-        const scrollPos = window.scrollY || window.scrollTop || document.getElementsByTagName("html")[0].scrollTop
-        
-
-        if(scrollPos > 10){
-            headerImg.style.height= '12vh'
-        } else if (scrollPos == 0){
-            headerImg.style.height= '15vh'
-        }      
-    })
-
-    
+    if (scrollPos > 10) {
+      headerImg.style.height = '12vh';
+    } else if (scrollPos == 0) {
+      headerImg.style.height = '15vh';
+    }
+  });
 }
 
-modifySizeOnScroll()
+modifySizeOnScroll();

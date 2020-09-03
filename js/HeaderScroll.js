fc06bda30374151ee646d38b9ee852141d17a789
Gabriell Castro de Carvalho <49693579+GabriellCdeC@users.noreply.github.com>
2020-09-02 22:12:29 -0300

    Update

--- js/HeaderScroll.js
+++ js/HeaderScroll.js
@@ -1,16 +1,22 @@
 "use strict";
 
-var headerElement = document.querySelector('#conteudo .header');
-var headerImg = document.querySelector('#conteudo .header_img img');
+var headerElement = document.querySelector('#conteudo .header')
+var headerImg = document.querySelector('#conteudo .header_img img')
+var headerTxt = document.querySelectorAll('#conteudo .text_header a')
+
 
 function modifySizeOnScroll() {
   window.addEventListener('scroll', function (event) {
     var scrollPos = window.scrollY || window.scrollTop || document.getElementsByTagName("html")[0].scrollTop;
 
-    if (scrollPos > 10) {
-      headerImg.style.height = '12vh';
-    } else if (scrollPos == 0) {
-      headerImg.style.height = '15vh';
+    for(const headerTxtelement of headerTxt){
+        if (scrollPos > 10) {
+            headerImg.style.height = '12vh';
+            headerTxtelement.style.fontSize = '1em';
+          }else if (scrollPos == 0) {
+            headerImg.style.height = '15vh';
+            headerTxtelement.style.fontSize = '1.25em';
+          }      
     }
   });
 }

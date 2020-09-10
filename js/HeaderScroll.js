312ff50b35b606793e470fb5d660b69c7ef0d8ef
Gabriell Castro de Carvalho <49693579+GabriellCdeC@users.noreply.github.com>
2020-09-10 11:11:18 -0300

    Update HeaderScroll.js

--- js/HeaderScroll.js
+++ js/HeaderScroll.js
@@ -24,3 +24,36 @@ function modifySizeOnScroll() {
 }
 
 modifySizeOnScroll();
+
+/*
+const headerElement = document.querySelector('#conteudo .header')
+const headerImg = document.querySelector('#conteudo .header_img img')
+
+
+function modifySizeOnScroll(){
+
+
+    window.addEventListener('scroll' , function(event){      
+
+        const scrollPos = window.scrollY || window.scrollTop || document.getElementsByTagName("html")[0].scrollTop
+        
+        if(window.screen.width > 1100){
+          if(scrollPos > 10){
+            headerImg.style.height= '12vh'
+          } else if (scrollPos == 0){
+            headerImg.style.height= '15vh'
+          } 
+        }else if(window.screen.width <= 1100){
+          if(scrollPos > 10){
+            headerImg.style.height= '11vh'
+          } else if (scrollPos == 0){
+            headerImg.style.height= '13vh'
+          } 
+        }
+             
+    })
+
+    
+}
+
+modifySizeOnScroll() */

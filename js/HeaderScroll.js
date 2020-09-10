ed9647e68915bcd45d830c7ba1904ced3983c01a
Gabriell Castro de Carvalho <49693579+GabriellCdeC@users.noreply.github.com>
2020-09-10 11:10:48 -0300

    Update HeaderScroll.js

--- js/HeaderScroll.js
+++ js/HeaderScroll.js
@@ -1,31 +1,26 @@
-const headerElement = document.querySelector('#conteudo .header')
-const headerImg = document.querySelector('#conteudo .header_img img')
+"use strict";
 
+var headerElement = document.querySelector('#conteudo .header');
+var headerImg = document.querySelector('#conteudo .header_img img');
 
-function modifySizeOnScroll(){
+function modifySizeOnScroll() {
+  window.addEventListener('scroll', function (event) {
+    var scrollPos = window.scrollY || window.scrollTop || document.getElementsByTagName("html")[0].scrollTop;
 
-
-    window.addEventListener('scroll' , function(event){      
-
-        const scrollPos = window.scrollY || window.scrollTop || document.getElementsByTagName("html")[0].scrollTop
-        
-        if(window.screen.width > 1100){
-          if(scrollPos > 10){
-            headerImg.style.height= '12vh'
-          } else if (scrollPos == 0){
-            headerImg.style.height= '15vh'
-          } 
-        }else if(window.screen.width <= 1100){
-          if(scrollPos > 10){
-            headerImg.style.height= '11vh'
-          } else if (scrollPos == 0){
-            headerImg.style.height= '13vh'
-          } 
-        }
-             
-    })
-
-    
+    if (window.screen.width > 1100) {
+      if (scrollPos > 10) {
+        headerImg.style.height = '12vh';
+      } else if (scrollPos == 0) {
+        headerImg.style.height = '15vh';
+      }
+    } else if (window.screen.width <= 1100) {
+      if (scrollPos > 10) {
+        headerImg.style.height = '11vh';
+      } else if (scrollPos == 0) {
+        headerImg.style.height = '13vh';
+      }
+    }
+  });
 }
 
-modifySizeOnScroll()
+modifySizeOnScroll();

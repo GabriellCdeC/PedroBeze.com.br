dd5e34dfb3844c294184c803f21b759b93e21f8e
Gabriell Castro de Carvalho <49693579+GabriellCdeC@users.noreply.github.com>
2020-09-15 17:18:43 -0300

    menu mobile

--- js/mobileHamburgerMenu.js
+++ js/mobileHamburgerMenu.js
@@ -1,10 +1,22 @@
-const elements = document.querySelectorAll('#menu li')
-const menu = document.querySelector('#menuToggle')
+const menu = document.querySelector('.menu')
+const btnMenu_open = document.querySelector('.btnMenu_open')
+const btnMenu_close = document.querySelector('.btnMenu_close')
+const btnMenu_itens = document.querySelectorAll('.menu-item-action')
 
-console.log(menu)
 
-for(const element of elements){
-    element.addEventListener('click', function(event){
-            menu.style.zIndex = 0
+
+btnMenu_open.addEventListener('click', function(event){
+    menu.classList.add('menu_open')
+})
+
+btnMenu_close.addEventListener('click', function(event){
+    menu.classList.remove('menu_open')
+})
+
+
+for(const item of btnMenu_itens){
+    item.addEventListener('click', function(event){
+        menu.classList.remove('menu_open')
     })
 }
+
